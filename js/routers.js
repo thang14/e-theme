25e7ea7657bb704a792117dd3d511e1a82976420
Nguyen Thang <thangn.1411@gmail.com>
2015-10-14 20:47:54 +0700

    Update routers.js

--- js/routers.js
+++ js/routers.js
@@ -10,6 +10,34 @@ onhanhApp.config(['$routeProvider',
         templateUrl: 'web/partials/product-detail.html',
         controller: 'ProductDetailController'
       }).
+      when('/reports', {
+        templateUrl: 'web/partials/reports-details.html',
+        controller: 'ReportsDetailController'
+      }).
+      when('/info', {
+        templateUrl: 'web/partials/shop-detail.html',
+        controller: 'ShopDetailController'
+      }).
+      when('/sections/', {
+        templateUrl: 'web/partials/section-list.html',
+        controller: 'SectionListController'
+      }).
+      when('/sections/:sectionId', {
+        templateUrl: 'web/partials/section-detail.html',
+        controller: 'SectionDetailController'
+      }).
+      when('/settings', {
+        templateUrl: 'web/partials/settings.html',
+        controller: 'SettingsController'
+      }).
+      when('/orders', {
+        templateUrl: 'web/partials/order-list.html',
+        controller: 'OrderListController'
+      }).
+      when('/orders/:orderId', {
+        templateUrl: 'web/partials/order-detail.html',
+        controller: 'OrderDetailController'
+      }).
       otherwise({
         redirectTo: '/'
       });
