2e637a3d73dc7521499e0198695389c7de95a744
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 10:13:31 +0700

    Update auth.js

---
+++ js/onhanh/auth/auth.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @name            OnhanhAuth
+ * @description     Auth module
+ */
+
+define(['app',
+    './config/main',
+    './controllers/auth',
+    './services/auth',
+], function(app) {
+    console.log('Auth module loaded ...');
+});
