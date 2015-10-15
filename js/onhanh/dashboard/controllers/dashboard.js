d90b90ee59fe7856271d5ed7da11b167ece430d8
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 12:18:36 +0700

    Update dashboard.js

---
+++ js/onhanh/dashboard/controllers/dashboard.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @name            OnhanhDashboard
+ * @description     ...
+ */
+
+define(['app'], function(app) {
+    app.controller('DashboardController', ['$location', '$scope', '$routeParams', '$rootScope',
+        function($location, $scope, $routeParams, $rootScope) {}
+    ]);
+});
