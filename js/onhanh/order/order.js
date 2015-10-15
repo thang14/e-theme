415fa94116ac707164f0b204327d47a67befbbe2
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 10:09:54 +0700

    Update order.js

---
+++ js/onhanh/order/order.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @name            OnhanhProduct
+ * @description     Product module
+ */
+
+define(['app',
+    './config/main',
+    './controllers/list',
+    './controllers/add',
+    './controllers/detail',
+], function(app) {
+    console.log('Order module loaded ...');
+});
