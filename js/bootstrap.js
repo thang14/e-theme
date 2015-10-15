f96e0ad2817cf91e0662ea330a2f2022fe5838c0
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 16:10:21 +0700

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4,14 +4,6 @@
  * @name            Onhanh
  * @description     Application Bootstrap File
  */
-
-define([
-    'jquery',
-    'app',
-], function(jQuery) {
-    'use strict';
-    
-    jQuery(document).ready(function() {
-      angular.bootstrap(document, ['app']);
-    });
+jQuery(document).ready(function() {
+  angular.bootstrap(document, ['app']);
 });
