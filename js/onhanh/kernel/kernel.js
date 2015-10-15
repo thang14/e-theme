4c88d507f0de1a932520aee7c10c1a103b23d312
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 10:20:22 +0700

    Update kernel.js

---
+++ js/onhanh/kernel/kernel.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @name            OnhanhKernel
+ * @description     Kernel module
+ */
+
+define(['app',
+    './config/main',
+    './controllers/abstractDetails',
+    './services/resource',
+], function(app) {
+    console.log('Kernel module loaded ...');
+});
