80050b72f057d11b2177f98b253c74108fc51d3c
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 10:15:26 +0700

    Update reports.js

---
+++ js/onhanh/reports/reports.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @name            OnhanhReports
+ * @description     Reports module
+ */
+
+define(['app',
+    './config/main',
+    './controllers/reports',
+    './services/reports',
+], function(app) {
+    console.log('Reports module loaded ...');
+});
