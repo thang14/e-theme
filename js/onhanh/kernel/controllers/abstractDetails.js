1aa96f3aeebb42a115f911261e144a99119d04d6
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 17:05:22 +0700

    Update abstractDetails.js

---
+++ js/onhanh/kernel/controllers/abstractDetails.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @name            OnhanhKernel
+ * @description     AbstractDetailsController
+ */
+
+angular.module('app.kernel')
+.controller('abstractDetailsController', ['$controller', '$scope', 'itemService', '$stateParams'
+    function($controller, $scope, itemService, $stateParams) {
+    
+    }
+]);
