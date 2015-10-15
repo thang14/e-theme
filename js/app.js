9678ce11d6c7647fa5ec6751d63791277d0d5b47
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 21:19:10 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
+'use strict';
 /**
  * @name            Onhanh
  * @description     App
  */
 
-(function() {
-	'use strict';
-	angular.module('app', [
-	    'app.auth',
-	    'app.environment',
-	    'app.product',
-	    'app.shop',
-	    'app.section',
-	    'app.reports',
-	    'app.kernel',
-	    'app.settings',
-	    'app.variant',
-	    'app.order',
-	    'app.dashboard'
-	]);
-})();
+angular.module('app', [
+    'app.auth',
+    'app.environment',
+    'app.product',
+    'app.shop',
+    'app.section',
+    'app.reports',
+    'app.kernel',
+    'app.settings',
+    'app.variant',
+    'app.order',
+    'app.dashboard'
+]);
