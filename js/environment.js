36ce6ce67a29dc545c7d8ec4b86935c1cf6e2e6a
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 09:40:43 +0700

    Update environment.js

---
+++ js/environment.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @name            Onhanh
+ * @description     Environment vars
+ */
+
+define(['angular'],
+    function(angular) {
+        'use strict';
+        console.log('Environment loaded ...');
+        angular.module('environment', [])
+            .service('Environment', function() {
+                var api_domain = document.domain.replace("admin", "api");
+
+                return {
+                    settings: {
+                        api: 'http://' + api_domain + '/backend/api',
+                        domain: 'http://' + api_domain,
+                        pageTitle: 'Aisel - open source project | Admin',
+                        locale: {
+                            "primary": 'en',
+                            "available": ['ru', 'es', 'en']
+                        }
+                    },
+                    currentLocale: function() {
+                        var locale = window.location.pathname.replace(/^\/([^\/]*).*$/, '$1');
+                        if (this.settings.locale.available.indexOf(locale) == -1) {
+                            locale = this.settings.locale.primary;
+                        }
+                        return locale;
+                    }
+                }
+            })
+    });
