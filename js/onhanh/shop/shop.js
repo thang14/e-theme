ef28ae3d981e92952fce44267b2a87f69240e848
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 10:16:51 +0700

    Update shop.js

---
+++ js/onhanh/shop/shop.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @name            OnhanhShop
+ * @description     Shop module
+ */
+
+define(['app',
+    './config/main',
+    './controllers/shop',
+    './services/shop',
+], function(app) {
+    console.log('Module shop loaded ...');
+});
