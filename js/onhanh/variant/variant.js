8b04a2946f9b96e335eec53b75f2d393150006d9
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 10:11:31 +0700

    Update variant.js

---
+++ js/onhanh/variant/variant.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @name            OnhanhVariant
+ * @description     Variant module
+ */
+
+define(['app',
+    './config/main',
+    './controllers/list',
+    './controllers/add',
+    './controllers/detail',
+], function(app) {
+    console.log('Order variant loaded ...');
+});
