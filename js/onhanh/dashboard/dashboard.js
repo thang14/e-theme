4db4e31b1e29a4d2811497f0174b794b8c50f585
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 10:14:53 +0700

    Update dashboard.js

---
+++ js/onhanh/dashboard/dashboard.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @name            OnhanhDashboard
+ * @description     Dashboard module
+ */
+
+define(['app',
+    './config/main',
+    './controllers/dashboard',
+    './services/dashboard',
+], function(app) {
+    console.log('Dashboard module loaded ...');
+});
