d6ac59db17c717883e1775588274a94473a7ab18
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 10:07:57 +0700

    Update product.js

---
+++ js/onhanh/product/product.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @name            OnhanhProduct
+ * @description     Product module
+ */
+
+define(['app',
+    './config/main',
+    './controllers/list',
+    './controllers/detail',
+], function(app) {
+    console.log('Product module loaded ...');
+});
