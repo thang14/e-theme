78541584c47c9d87fdb4f5df0938040be9671495
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 10:10:39 +0700

    Update section.js

---
+++ js/onhanh/section/section.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @name            OnhanhSection
+ * @description     Section module
+ */
+
+define(['app',
+    './config/main',
+    './controllers/list',
+    './controllers/add',
+    './controllers/detail',
+], function(app) {
+    console.log('Section module loaded ...');
+});
