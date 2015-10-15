154941f680a9fb3e31a81955566e335b7ab4cfd0
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 22:14:36 +0700

    Update adminFiles.js

--- adminFiles.js
+++ adminFiles.js
@@ -1,137 +1,33 @@
 'use strict';
 
 var angularFiles = {
-  'angularSrc': [
-    'src/minErr.js',
-    'src/Angular.js',
-    'src/loader.js',
-    'src/stringify.js',
-    'src/AngularPublic.js',
-    'src/jqLite.js',
-    'src/apis.js',
-
-    'src/auto/injector.js',
-
-    'src/ng/anchorScroll.js',
-    'src/ng/animate.js',
-    'src/ng/animateCss.js',
-    'src/ng/browser.js',
-    'src/ng/cacheFactory.js',
-    'src/ng/compile.js',
-    'src/ng/controller.js',
-    'src/ng/document.js',
-    'src/ng/exceptionHandler.js',
-    'src/ng/forceReflow.js',
-    'src/ng/http.js',
-    'src/ng/httpBackend.js',
-    'src/ng/interpolate.js',
-    'src/ng/interval.js',
-    'src/ng/locale.js',
-    'src/ng/location.js',
-    'src/ng/log.js',
-    'src/ng/parse.js',
-    'src/ng/q.js',
-    'src/ng/raf.js',
-    'src/ng/rootScope.js',
-    'src/ng/sanitizeUri.js',
-    'src/ng/sce.js',
-    'src/ng/sniffer.js',
-    'src/ng/templateRequest.js',
-    'src/ng/testability.js',
-    'src/ng/timeout.js',
-    'src/ng/urlUtils.js',
-    'src/ng/window.js',
-    'src/ng/cookieReader.js',
-
-    'src/ng/filter.js',
-    'src/ng/filter/filter.js',
-    'src/ng/filter/filters.js',
-    'src/ng/filter/limitTo.js',
-    'src/ng/filter/orderBy.js',
-
-    'src/ng/directive/directives.js',
-    'src/ng/directive/a.js',
-    'src/ng/directive/attrs.js',
-    'src/ng/directive/form.js',
-    'src/ng/directive/input.js',
-    'src/ng/directive/ngBind.js',
-    'src/ng/directive/ngChange.js',
-    'src/ng/directive/ngClass.js',
-    'src/ng/directive/ngCloak.js',
-    'src/ng/directive/ngController.js',
-    'src/ng/directive/ngCsp.js',
-    'src/ng/directive/ngEventDirs.js',
-    'src/ng/directive/ngIf.js',
-    'src/ng/directive/ngInclude.js',
-    'src/ng/directive/ngInit.js',
-    'src/ng/directive/ngList.js',
-    'src/ng/directive/ngModel.js',
-    'src/ng/directive/ngNonBindable.js',
-    'src/ng/directive/ngOptions.js',
-    'src/ng/directive/ngPluralize.js',
-    'src/ng/directive/ngRepeat.js',
-    'src/ng/directive/ngShowHide.js',
-    'src/ng/directive/ngStyle.js',
-    'src/ng/directive/ngSwitch.js',
-    'src/ng/directive/ngTransclude.js',
-    'src/ng/directive/script.js',
-    'src/ng/directive/select.js',
-    'src/ng/directive/style.js',
-    'src/ng/directive/validators.js',
-    'src/angular.bind.js',
-    'src/publishExternalApis.js',
-    'src/ngLocale/angular-locale_en-us.js'
-  ],
-
-  'angularLoader': [
-    'stringify.js',
-    'src/minErr.js',
-    'src/loader.js'
-  ],
-
-  'angularModules': {
-    'ngAnimate': [
-      'src/ngAnimate/shared.js',
-      'src/ngAnimate/rafScheduler.js',
-      'src/ngAnimate/animateChildrenDirective.js',
-      'src/ngAnimate/animateCss.js',
-      'src/ngAnimate/animateCssDriver.js',
-      'src/ngAnimate/animateJs.js',
-      'src/ngAnimate/animateJsDriver.js',
-      'src/ngAnimate/animateQueue.js',
-      'src/ngAnimate/animateRunner.js',
-      'src/ngAnimate/animation.js',
-      'src/ngAnimate/module.js'
+  'modules': {
+    'kernel': [
+      'js/kernel/kernel.js',
     ],
-    'ngCookies': [
-      'src/ngCookies/cookies.js',
-      'src/ngCookies/cookieStore.js',
-      'src/ngCookies/cookieWriter.js'
+    'product': [
+      'js/product/product.js',
     ],
-    'ngMessageFormat': [
-      'src/ngMessageFormat/messageFormatCommon.js',
-      'src/ngMessageFormat/messageFormatSelector.js',
-      'src/ngMessageFormat/messageFormatInterpolationParts.js',
-      'src/ngMessageFormat/messageFormatParser.js',
-      'src/ngMessageFormat/messageFormatService.js'
+    'auth': [
+      'js/auth/auth.js',
     ],
-    'ngMessages': [
-      'src/ngMessages/messages.js'
+    'variant': [
+      'js/variant/variant.js'
     ],
-    'ngResource': [
-      'src/ngResource/resource.js'
+    'shop': [
+      'js/shop/shop.js'
     ],
-    'ngRoute': [
-      'src/ngRoute/route.js',
-      'src/ngRoute/routeParams.js',
-      'src/ngRoute/directive/ngView.js'
+    'section': [
+      'js/shop/section.js'
     ],
-    'ngSanitize': [
-      'src/ngSanitize/sanitize.js',
-      'src/ngSanitize/filter/linky.js'
+    'order': [
+      'js/order/order.js'
     ],
-    'ngMock': [
-      'src/ngMock/angular-mocks.js'
+    'settings': [
+      'js/settings/settings.js'
+    ],
+    'dashboard': [
+      'src/dashboard/dashboard.js'
     ],
     'ngTouch': [
       'src/ngTouch/touch.js',
