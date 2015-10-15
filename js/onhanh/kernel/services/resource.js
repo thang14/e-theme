d0f124ab21b22f5cc140b1c9ac6e35bbba90113e
Nguyen Thang <thangn.1411@gmail.com>
2015-10-15 16:15:15 +0700

    Update resource.js

--- js/onhanh/kernel/services/resource.js
+++ js/onhanh/kernel/services/resource.js
@@ -4,47 +4,43 @@
  * @name            OnhanhKernel
  * @description     resourceService
  */
-define([
-    'app'
-],function(app) {
-    app.factory('resourceService', ['$http', 'Environment',
-        function($http, Environment) {
-        
-            var resourceService = function(name) {
-                this.resource = name;
-            }
-            
-            resourceService.prototype = {
-            
-                getCollection: function($scope, pageNumber) {
-                    var url = Environment.settings.api + '/' + this.resource + '/?limit=' + $scope.pageLimit + '&current=' + pageNumber + '&filter=' + $scope.filter;
-                    return $http.get(url);
-                }, 
-                
-                remove: function($id) {
-                    var url = Environment.settings.api+'/'+this.resource+'/'+$id;
-                    return $http.delete(url);
-                },
-                
-                save: function(data) {
-                    var url = Environment.settings.api+'/'+this.resource+'/'+$id;
-                    return $http({
-                        method: 'PUT',
-                        url: url,
-                        data: data
-                    });
-                },
-                
-                create: function(data) {
-                    var url = Environment.settings.api+'/'+this.resource+'/'+$id;
-                    return $http({
-                        method: 'POST',
-                        url: url,
-                        data: data
-                    });
-                },
-                
-            }
+angular.module('app.kernel').factory('resourceService', ['$http', 'Environment',
+    function($http, Environment) {
+    
+        var resourceService = function(name) {
+            this.resource = name;
         }
-    ]);
-});
+        
+        resourceService.prototype = {
+        
+            getCollection: function($scope, pageNumber) {
+                var url = Environment.settings.api + '/' + this.resource + '/?limit=' + $scope.pageLimit + '&current=' + pageNumber + '&filter=' + $scope.filter;
+                return $http.get(url);
+            }, 
+            
+            remove: function($id) {
+                var url = Environment.settings.api+'/'+this.resource+'/'+$id;
+                return $http.delete(url);
+            },
+            
+            save: function(data) {
+                var url = Environment.settings.api+'/'+this.resource+'/'+$id;
+                return $http({
+                    method: 'PUT',
+                    url: url,
+                    data: data
+                });
+            },
+            
+            create: function(data) {
+                var url = Environment.settings.api+'/'+this.resource+'/'+$id;
+                return $http({
+                    method: 'POST',
+                    url: url,
+                    data: data
+                });
+            },
+            
+        }
+    }
+]);
