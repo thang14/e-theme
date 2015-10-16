cab622e5639f9bafbab5c10c4fe0e83e6e66c770
Nguyen Thang <thangn.1411@gmail.com>
2015-10-16 21:00:52 +0700

    Update main.js

---
+++ js/shop/config/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @name            OnhanhShop
+ * @description     ShopConfig
+ */
+shopModule
+    .config(['$stateProvider',
+        function($stateProvider) {
+         // Use $stateProvider to configure your states.
+          $stateProvider
+
+            .state("shop", {
+              title: "Shop",
+              // Use a url of "/" to set a states as the "index".
+              url: "/shop",
+
+              // Example of an inline template string. By default, templates
+              // will populate the ui-view within the parent state's template.
+              // For top level states, like this one, the parent template is
+              // the index.html file. So this template will be inserted into the
+              // ui-view within index.html.
+              controller: 'shopController',
+              templateUrl: '/web/shop/shop.html',
+            });
+
+
+
+            
+        }
+    ]);
