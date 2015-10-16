c046013edc9dd96cdc1640b9cc10c62cbb0dd7f5
Nguyen Thang <thangn.1411@gmail.com>
2015-10-16 20:47:39 +0700

    Update main.js

---
+++ js/order/config/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @name            OnhanhOrder
+ * @description     OrderConfig
+ */
+productModule
+    .config(['$stateProvider',
+        function($stateProvider) {
+         // Use $stateProvider to configure your states.
+          $stateProvider
+
+            .state("order", {
+              title: "Order",
+              // Use a url of "/" to set a states as the "index".
+              url: "/order",
+
+              // Example of an inline template string. By default, templates
+              // will populate the ui-view within the parent state's template.
+              // For top level states, like this one, the parent template is
+              // the index.html file. So this template will be inserted into the
+              // ui-view within index.html.
+              controller: 'orderController',
+              templateUrl: '/web/order/list.html',
+            });
+
+
+
+            
+        }
+    ]);
