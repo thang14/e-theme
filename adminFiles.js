36a968b428f9591c9d978bbe0fe4f1f2bcd623a1
Nguyen Thang <thangn.1411@gmail.com>
2015-10-16 19:48:26 +0700

    Update adminFiles.js

--- adminFiles.js
+++ adminFiles.js
@@ -67,7 +67,7 @@ var adminFiles = {
     //Components
     'breadcrumb' => [
       'js/breadcrumb/breadcrumb.js',
-      'js/breadcrumb/directive.js'
+      'js/breadcrumb/config/**.js'
     ]
   },
 };
