e1ce3b3ac5a9fced77256741d4694b2663a70bc6
Nguyen Thang <thangn.1411@gmail.com>
2015-10-16 17:21:19 +0700

    Update directive.js

--- js/breadcrumb/directive.js
+++ js/breadcrumb/directive.js
@@ -1,1 +1,13 @@
+'use strict';
 
+/**
+ * @name            OnhanhBreadcrumb
+ * @description     BreadcrumbModule
+ */
+breadcrumbModule.directive('breadcrumbs', ['$state'
+    function($state) {
+        return {
+            templateUrl:"/web/breadcrumb/breadcrumb"
+        }
+    }
+]);
