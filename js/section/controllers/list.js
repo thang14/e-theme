4b222c4addd9491fda7ccc01e35c4e5d6c15f842
Nguyen Thang <thangn.1411@gmail.com>
2015-10-16 20:58:46 +0700

    Update list.js

---
+++ js/section/controllers/list.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @name            OnhanhSection
+ * @description     SectionController
+ */
+sectionModule
+    .controller('sectionController', [ '$scope',
+        function($scope) {
+            
+        }
+    ]);
