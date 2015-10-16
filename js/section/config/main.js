89ad1e3f673db53f62a63826153b8eb254eb7c1e
Nguyen Thang <thangn.1411@gmail.com>
2015-10-16 20:55:56 +0700

    Update main.js

---
+++ js/section/config/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @name            OnhanhSection
+ * @description     SectionConfig
+ */
+orderModule
+    .config(['$stateProvider',
+        function($stateProvider) {
+         // Use $stateProvider to configure your states.
+          $stateProvider
+
+            .state("section", {
+              title: "Section",
+              // Use a url of "/" to set a states as the "index".
+              url: "/section",
+
+              // Example of an inline template string. By default, templates
+              // will populate the ui-view within the parent state's template.
+              // For top level states, like this one, the parent template is
+              // the index.html file. So this template will be inserted into the
+              // ui-view within index.html.
+              controller: 'sectionController',
+              templateUrl: '/web/section/list.html',
+            });
+
+
+
+            
+        }
+    ]);
