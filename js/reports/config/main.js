a97ffa4e96a668c9fd9f43b58420db27b0e3e713
Nguyen Thang <thangn.1411@gmail.com>
2015-10-16 20:57:26 +0700

    Update main.js

--- js/reports/config/main.js
+++ js/reports/config/main.js
@@ -1,27 +1,27 @@
 'use strict';
 
 /**
- * @name            OnhanhSection
- * @description     SectionConfig
+ * @name            OnhanhReports
+ * @description     ReportsConfig
  */
-sectionModule
+reportsModule
     .config(['$stateProvider',
         function($stateProvider) {
          // Use $stateProvider to configure your states.
           $stateProvider
 
-            .state("section", {
-              title: "Section",
+            .state("reports", {
+              title: "Reports",
               // Use a url of "/" to set a states as the "index".
-              url: "/section",
+              url: "/reports",
 
               // Example of an inline template string. By default, templates
               // will populate the ui-view within the parent state's template.
               // For top level states, like this one, the parent template is
               // the index.html file. So this template will be inserted into the
               // ui-view within index.html.
-              controller: 'sectionController',
-              templateUrl: '/web/section/list.html',
+              controller: 'reportsController',
+              templateUrl: '/web/reports/list.html',
             });
 
 
