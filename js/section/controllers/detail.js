71b75dd8c68c6db31e20e637693a77cea9349683
Nguyen Thang <thangn.1411@gmail.com>
2015-10-17 10:43:41 +0700

    Update detail.js

---
+++ js/section/controllers/detail.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @name            OnhanhSection
+ * @description     SectionDetailController
+ */
+sectionModule
+    .controller('sectionDetailController', [ '$scope',
+        function($scope) {
+            
+        }
+    ]);
