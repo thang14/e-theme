53005ccdf30d196e1e099e135b2360963dc8ba56
Nguyen Thang <thangn.1411@gmail.com>
2015-10-17 10:45:59 +0700

    Update main.js

--- js/section/config/main.js
+++ js/section/config/main.js
@@ -22,7 +22,7 @@ sectionModule
               // ui-view within index.html.
               controller: 'sectionController',
               templateUrl: '/web/section/list.html',
-            });
+            })
 
             .state("section.new", {
               title: "Section",
@@ -42,6 +42,22 @@ sectionModule
               }
             });
 
-            
+            .state("section.detail", {
+              title: "Section detail",
+              // Use a url of "/" to set a states as the "index".
+              url: "/:sectionId",
+
+              views: {
+                '@': {
+                    // Example of an inline template string. By default, templates
+                    // will populate the ui-view within the parent state's template.
+                    // For top level states, like this one, the parent template is
+                    // the index.html file. So this template will be inserted into the
+                    // ui-view within index.html.
+                    controller: 'sectionDetailController',
+                    templateUrl: '/web/section/detail.html',
+                }  
+              }
+            });
         }
     ]);
