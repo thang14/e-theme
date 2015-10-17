26a7ec4401413fb3151adbad24669296a1317d59
Nguyen Thang <thangn.1411@gmail.com>
2015-10-17 10:38:08 +0700

    Update list.js

---
+++ js/variant/controllers/list.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @name            OnhanhVariant
+ * @description     VariantListController
+ */
+variantModule
+	.controller('variantController', ['$location', '$scope', '$rootScope',
+	    function($location, $scope, $rootScope) {
+	    	
+	    }
+	]);
