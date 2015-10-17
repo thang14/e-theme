6572ef713b04691ea601486d651c14e75986f567
Nguyen Thang <thangn.1411@gmail.com>
2015-10-17 10:34:05 +0700

    Update main.js

--- js/product/config/main.js
+++ js/product/config/main.js
@@ -56,7 +56,7 @@ productModule
             .state("product.detail", {
               title: "Product detail",
               // Use a url of "/" to set a states as the "index".
-              url: "/:id",
+              url: "/:productId",
 
               views: {
                   "@" : {
@@ -67,6 +67,48 @@ productModule
                       // ui-view within index.html.
                       controller: 'productDetailController',
                       templateUrl: '/web/product/detail.html',
+                  }
+              },
+            })
+            
+            //////////////////
+            // Product Variant //
+            ////////////////
+            .state("product.detail.variant", {
+              title: "Variant list",
+              // Use a url of "/" to set a states as the "index".
+              url: "/:productId/variant",
+
+              views: {
+                  "@" : {
+                      // Example of an inline template string. By default, templates
+                      // will populate the ui-view within the parent state's template.
+                      // For top level states, like this one, the parent template is
+                      // the index.html file. So this template will be inserted into the
+                      // ui-view within index.html.
+                      controller: 'variantController',
+                      templateUrl: '/web/variant/detail.html',
+                  }
+              },
+            }).
+            
+            //////////////////
+            // Product Variant detail //
+            ////////////////
+            .state("product.detail.variant.detail", {
+              title: "Variant list",
+              // Use a url of "/" to set a states as the "index".
+              url: "/variant/:variantId",
+
+              views: {
+                  "@" : {
+                      // Example of an inline template string. By default, templates
+                      // will populate the ui-view within the parent state's template.
+                      // For top level states, like this one, the parent template is
+                      // the index.html file. So this template will be inserted into the
+                      // ui-view within index.html.
+                      controller: 'variantController',
+                      templateUrl: '/web/variant/detail.html',
                   }
               },
             });
