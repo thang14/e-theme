dd3204841fec19f681c25b494d1b347acbf5e437
Nguyen Thang <thangn.1411@gmail.com>
2015-10-17 10:47:58 +0700

    Update add.js

---
+++ js/section/controllers/add.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @name            OnhanhSection
+ * @description     SectionAddController
+ */
+sectionModule
+    .controller('sectionAddController', [ '$scope',
+        function($scope) {
+            
+        }
+    ]);
