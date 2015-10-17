438fb1832330ec288193669893b92985e02e334d
Nguyen Thang <thangn.1411@gmail.com>
2015-10-17 10:38:44 +0700

    Update add.js

---
+++ js/variant/controllers/add.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @name            OnhanhVariant
+ * @description     VariantAddController
+ */
+variantModule
+	.controller('variantAddController', ['$location', '$scope', '$rootScope',
+	    function($location, $scope, $rootScope) {
+	    	
+	    }
+	]);
