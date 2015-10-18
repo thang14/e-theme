dafc99518d6c564f9c6f63e8f1b246db1b2feb55
Nguyen Thang <thangn.1411@gmail.com>
2015-10-18 16:23:51 +0700

    Update list.js

--- js/product/controllers/list.js
+++ js/product/controllers/list.js
@@ -5,18 +5,9 @@
  * @description     ProductController
  */
 productModule
-	.controller('productController', ['$location', '$scope', '$rootScope',
-	    function($location, $scope, $rootScope) {
-			$scope.gridOptions = {
-				data: []
-			};
-			for(var i = 0; i < 10; i++) {
-				$scope.gridOptions.data.push({
-					name: "Ấm chén tử sa "+i,
-					price: "150000",
-					sale: "150000",
-					quantity: 11,
-				});
-			}
+	.controller('productController', ['$location', '$scope', '$rootScope', 'collectionService', 'gridService'
+	    function($location, $scope, $rootScope, collectionService) {
+	    	var collection = collectionService.getCollection('product');
+	    	$scope.gripOptions = new gridService(collection);
 	    }
 	]);
