b277aebe6c20b1b743bc7b09b823274c7bdf266a
Nguyen Thang <thangn.1411@gmail.com>
2015-10-18 18:32:58 +0700

    Update list.js

--- js/product/controllers/list.js
+++ js/product/controllers/list.js
@@ -8,6 +8,15 @@ productModule
 	.controller('productController', ['$location', '$scope', '$rootScope', 'collectionService', 'gridService'
 	    function($location, $scope, $rootScope, collectionService) {
 	    	var collection = collectionService.getCollection('product');
+	    	$scope.columns = [{
+	    		name: "photo",
+	    		enableSorting: false,
+                enableFiltering: false,
+                enableColumnMenu: false,
+                width: '75',
+                cellTemplate: '<img src="row.entity.photo"/>'
+	    	}];
+	    	
 	    	$scope.gripOptions = gridService.gripOptions();
 	    	
 	    	// === Load collection from remote ===
