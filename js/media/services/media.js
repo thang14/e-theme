7cf6d925a2957ac7efbf712962b3a080481028c5
Nguyen Thang <thangn.1411@gmail.com>
2015-10-18 19:22:01 +0700

    Create media.js

---
+++ js/media/services/media.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @name            OnhanhMedia
+ * @description     MediaService
+ */
+mediaModule.service('mediaService', [ 'Upload', 'collectionService'
+    function(Upload, collectionService) {
+        return {
+            getCollection: function() {
+                return collectionService.getCollection('media');
+            },
+            
+            upload: function($file, callback) {
+                var api = this.getCollection().api;
+                return Upload.upload(api).success(callback);
+            },
+            
+            uploadAll: function($files, callback) {
+                $files.forEach(function(file) {
+                    this.upload(file, callback);
+                }, this);
+            },
+            
+            remove: function($id) {
+                return this.getCollection()
+                            .remove($id);
+            }
+        }
+    }
+]);
