410b35efab0db9edf6810a9d6642e33bb8e47841
Nguyen Thang <thangn.1411@gmail.com>
2015-10-19 08:43:46 +0700

    Update environment.js

--- js/environment.js
+++ js/environment.js
@@ -12,19 +12,9 @@ angular.module('app.environment', [])
         return {
             settings: {
                 api: 'http://' + api_domain + '/admin-theme/examples/data',
+                prefix: ".json",
                 domain: 'http://' + api_domain,
-                pageTitle: 'Onhanh - open source project | Admin',
-                locale: {
-                    "primary": 'en',
-                    "available": ['ru', 'es', 'en']
-                }
-            },
-            currentLocale: function() {
-                var locale = window.location.pathname.replace(/^\/([^\/]*).*$/, '$1');
-                if (this.settings.locale.available.indexOf(locale) == -1) {
-                    locale = this.settings.locale.primary;
-                }
-                return locale;
+                pageTitle: 'Onhanh - open source project | Admin'
             }
         }
     });
