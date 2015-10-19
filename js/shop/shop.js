013d88034d898993df84ac59dfa64ba0956aa8da
Nguyen Thang <thangn.1411@gmail.com>
2015-10-20 06:39:43 +0700

    Update shop.js

--- js/shop/shop.js
+++ js/shop/shop.js
@@ -4,4 +4,27 @@
  * @name            OnhanhShop
  * @description     ShopModule
  */
-var shopModule = angular.module("app.shop", []);
+var shopModule = angular.module("app.shop", [])
+.run(['$http', '$state', '$rootScope', 'shopService', 'Environment',
+  function($http, $state, $rootScope, authService, Environment) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+      if ($rootScope.shop === undefined) {
+        shopService
+        .get({})
+        .success(function(data, status) {
+          if(data.name) {
+            $rootScope.shop = data;
+          } else {
+            $rootScope.shop = false;
+            event.preventDefault();
+            $state.transitionTo('404');
+          }
+        });
+      } else if($rootScope.shop == false) {
+        event.preventDefault();
+        $state.transitionTo('404');
+      }
+    })
+ 
+  }
+]);
