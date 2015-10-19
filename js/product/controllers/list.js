2814e9160ce52e8a9dcd660dab7207c4226ff829
Nguyen Thang <thangn.1411@gmail.com>
2015-10-19 16:21:53 +0700

    Update list.js

--- js/product/controllers/list.js
+++ js/product/controllers/list.js
@@ -24,18 +24,19 @@ productModule
             name: "name",
             enableCellEdit: true,
             enableColumnMenu: false,
-            cellTemplate: '<a ng-click="viewDetail(row.entity.id)" href="javascript:void(0)">{{row.entity.name}}</a>'
+            cellTemplate: '<a ng-click="viewDetail(row.entity.id)" href="javascript:void(0)">{{row.entity.name}}</a>',
           },{
             name: "price",
             enableColumnMenu: false,
             width: '100',
             enableCellEdit: true,
-            editableCellTemplate: '',
+            editableCellTemplate: '<input ui-number-mask="0" type="text" ng-model="row.entity.price"/>',
           },{
             name: "sale_price",
             enableColumnMenu: false,
             enableCellEdit: true,
             width: '100',
+            editableCellTemplate: '<input ui-number-mask="0" type="text" ng-model="row.entity.sale_price"/>',
           },{
             name: "status",
             enableColumnMenu: false,
