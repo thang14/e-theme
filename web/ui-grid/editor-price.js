e58ff97d510a3e3efbe79f3cb0cb2d5ea429857f
Nguyen Thang <thangn.1411@gmail.com>
2015-10-19 17:20:34 +0700

    Update editor-price.js

--- web/ui-grid/editor-price.js
+++ web/ui-grid/editor-price.js
@@ -1,5 +1,5 @@
 <div>
   <form name="inputForm">
-    <input ng-class="'colt' + col.uid" type="INPUT_TYPE" ui-grid-editor ng-model="MODEL_COL_FIELD">
+    <input ui-number-mask ng-class="'colt' + col.uid" type="INPUT_TYPE" ui-grid-editor ng-model="MODEL_COL_FIELD">
   </form>
 </div>
