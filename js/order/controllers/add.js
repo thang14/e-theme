e80ab4a430006e3949729179693ea54e0a10f51b
Nguyen Thang <thangn.1411@gmail.com>
2015-10-19 08:13:26 +0700

    Update add.js

---
+++ js/order/controllers/add.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @name            OnhanhOrder
+ * @description     OrderAddController
+ */
+orderModule
+    .controller('orderAddController', [ '$scope', 'orderService'
+        function($scope, orderService) {
+            
+        }
+    ]);
