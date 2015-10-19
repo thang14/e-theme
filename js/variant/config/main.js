623f6e340c9c38a3315ed3b9b7feb119f68901ad
Nguyen Thang <thangn.1411@gmail.com>
2015-10-19 15:00:39 +0700

    Update main.js

--- js/variant/config/main.js
+++ js/variant/config/main.js
@@ -8,25 +8,5 @@
 variantModule
     .config(['$stateProvider',
         function($stateProvider) {
-         // Use $stateProvider to configure your states.
-          $stateProvider
-
-            .state("variant", {
-              title: "Variant",
-              // Use a url of "/" to set a states as the "index".
-              url: "/variant",
-
-              // Example of an inline template string. By default, templates
-              // will populate the ui-view within the parent state's template.
-              // For top level states, like this one, the parent template is
-              // the index.html file. So this template will be inserted into the
-              // ui-view within index.html.
-              controller: 'variantController',
-              templateUrl: '/web/variant/list.html',
-            });
-
-
-
-            
         }
     ]);
