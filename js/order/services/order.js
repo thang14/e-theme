3cc16e2085599e7691841d814add6f1e288caa06
Nguyen Thang <thangn.1411@gmail.com>
2015-10-19 08:16:54 +0700

    Update order.js

--- js/order/services/order.js
+++ js/order/services/order.js
@@ -5,7 +5,7 @@
  * @description     OrderServiceController
  */
 orderModule
-    .controller('orderServiceController', [ 'baseService'
+    .controller('orderService', [ 'baseService'
         function(baseService) {
           return angular.extend(baseService, {
             collectionName: "order"
