401d34bc6fee2165d729f57b7bd6aebb458b5654
Nguyen Thang <thangn.1411@gmail.com>
2015-10-19 11:05:47 +0700

    Update list.js

--- js/product/controllers/list.js
+++ js/product/controllers/list.js
@@ -1,1 +1,43 @@
+'use strict';
 
+/**
+ * @name            OnhanhProduct
+ * @description     ProductController
+ */
+productModule
+    .controller('productController', [ '$scope','productService', 'gridService'
+        function($scope, productService, gridService) {
+          $scope.columns = [{
+            name: "id",
+            enableColumnMenu: false,
+            enableSorting: false,
+            enableFiltering: false,
+            width: '75',
+          },{
+            name: "photo",
+            enableColumnMenu: false,
+            enableSorting: false,
+            enableFiltering: false,
+            width: '75',
+            cellTemplate: '<img ng-src="row.entity.photos"/>'
+          },{
+            name: "name",
+            enableColumnMenu: false,
+          },{
+            name: "price",
+            enableColumnMenu: false,
+            width: '100',
+          },{
+            name: "sale_price",
+            enableColumnMenu: false,
+            width: '100',
+          },{
+            name: "action",
+            enableColumnMenu: false,
+            enableSorting: false,
+            enableFiltering: false,
+            width: '75',
+            cellTemplate: gridService.gridOptions()
+          }];  
+        }
+    ]);
