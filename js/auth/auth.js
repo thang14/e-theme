4a64d80312fab5089311431aba21f1ac04c7467b
Nguyen Thang <thangn.1411@gmail.com>
2015-10-19 22:16:26 +0700

    Update auth.js

--- js/auth/auth.js
+++ js/auth/auth.js
@@ -5,4 +5,27 @@
  * @description     AuthModule
  */
  
-var authModule = angular.module('app.auth', []);
+var authModule = angular.module('app.auth', [])
+.run(['$http', '$state', '$rootScope', 'authService', 'Environment',
+  function($http, $state, $rootScope, authService, Environment) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+      if ($rootScope.user === undefined) {
+        authService
+        .getUserInfomation()
+        .success(function(data, status) {
+          if(data.username) {
+            $rootScope.user = data;
+          } else {
+            $rootScope.user = false;
+            event.preventDefault();
+            $state.transitionTo('userLogin');
+          }
+        });
+      } else if($rootScope.user == false) {
+        event.preventDefault();
+        $state.transitionTo('userLogin');
+      }
+    })
+ 
+  }
+]);
