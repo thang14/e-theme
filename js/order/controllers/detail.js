edda3ac6668e9b807c52f414a8230a7d954f18bb
Nguyen Thang <thangn.1411@gmail.com>
2015-10-19 08:14:13 +0700

    Update detail.js

---
+++ js/order/controllers/detail.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @name            OnhanhOrder
+ * @description     OrderDetailController
+ */
+orderModule
+    .controller('orderDetailController', [ '$scope', 'orderService'
+        function($scope, orderService) {
+            
+        }
+    ]);
