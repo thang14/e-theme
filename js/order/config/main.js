d7db2f860deeda944dabaf88cbaa0ae5db320431
Nguyen Thang <thangn.1411@gmail.com>
2015-10-20 09:32:21 +0700

    Update main.js

--- js/order/config/main.js
+++ js/order/config/main.js
@@ -22,6 +22,20 @@ orderModule
               // ui-view within index.html.
               controller: 'orderController',
               templateUrl: '/web/order/list.html',
+            })
+            
+            .state("order.detail", {
+              title: "Order Detail",
+              // Use a url of "/" to set a states as the "index".
+              url: "/:orderId",
+
+              // Example of an inline template string. By default, templates
+              // will populate the ui-view within the parent state's template.
+              // For top level states, like this one, the parent template is
+              // the index.html file. So this template will be inserted into the
+              // ui-view within index.html.
+              controller: 'orderDetailController',
+              templateUrl: '/web/order/detail.html',
             });
 
 
