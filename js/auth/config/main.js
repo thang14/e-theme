1bf514aa84cb7c08177c1992921ec6a144e70815
Nguyen Thang <thangn.1411@gmail.com>
2015-10-20 07:50:38 +0700

    Update main.js

--- js/auth/config/main.js
+++ js/auth/config/main.js
@@ -5,15 +5,50 @@
  * @name            OnhanhAuth
  * @description     Module config
  */
-authModule.config(['$stateProvider', 
-  function($stateProvider) {
-    $stateProvider.state('userLogin', {
-      url: "/user/login",
-      templateUrl: '/web/auth/login.html',
-      controller: 'AuthLoginController',
-      data: {
-        role: 'guest'
-      }
-    });
-  }
-]);
+ 
+authModule.
+'use strict';
+
+/**
+ * @name            OnhanhOrder
+ * @description     OrderConfig
+ */
+orderModule
+    .config(['$stateProvider',
+        function($stateProvider) {
+         // Use $stateProvider to configure your states.
+          $stateProvider
+
+            .state("user", {
+              title: "User",
+              // Use a url of "/" to set a states as the "index".
+              url: "/user",
+
+              // Example of an inline template string. By default, templates
+              // will populate the ui-view within the parent state's template.
+              // For top level states, like this one, the parent template is
+              // the index.html file. So this template will be inserted into the
+              // ui-view within index.html.
+              controller: 'authController',
+              templateUrl: '/web/auth/detail.html',
+            })
+            
+            .state("user.login", {
+              title: "Login",
+              // Use a url of "/" to set a states as the "index".
+              url: "/login",
+
+              // Example of an inline template string. By default, templates
+              // will populate the ui-view within the parent state's template.
+              // For top level states, like this one, the parent template is
+              // the index.html file. So this template will be inserted into the
+              // ui-view within index.html.
+              controller: 'authLoginController',
+              templateUrl: '/web/auth/login.html',
+            })
+
+
+            
+        }
+    ]);
+
