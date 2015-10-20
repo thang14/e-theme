92ab0cc78f28360b50729db8256a973f00ac8612
Nguyen Thang <thangn.1411@gmail.com>
2015-10-20 07:55:16 +0700

    Update list.js

--- js/order/controllers/list.js
+++ js/order/controllers/list.js
@@ -7,6 +7,30 @@
 orderModule
     .controller('orderController', [ '$scope', 'orderService'
         function($scope, orderService) {
-            
+            $scope.columns = [{
+                name: "id",
+                enableColumnMenu: false,
+                enableSorting: false,
+                enableFiltering: false,
+                width: '75',
+            },{
+                name: "email",
+                enableColumnMenu: false,
+                enableSorting: false,
+                enableFiltering: false,
+                width: '200',
+            },{
+                name: "price",
+                enableColumnMenu: false,
+                enableSorting: false,
+                enableFiltering: false,
+                width: '200',
+            },{
+                name: "status",
+                enableColumnMenu: false,
+                enableSorting: false,
+                enableFiltering: false,
+                width: '80',
+            }];
         }
     ]);
