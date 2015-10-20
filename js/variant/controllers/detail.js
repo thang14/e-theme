c9628161bedddd5a2df8c2d4e0e5587297df6ca2
Nguyen Thang <thangn.1411@gmail.com>
2015-10-20 15:11:55 +0700

    Update detail.js

--- js/variant/controllers/detail.js
+++ js/variant/controllers/detail.js
@@ -5,8 +5,23 @@
  * @description     VariantDetailController
  */
 variantModule
-	.controller('variantDetailController', ['$location', '$scope', '$rootScope',
-	    function($location, $scope, $rootScope) {
-	    	
+	.controller('variantDetailController', ['$location', '$scope', '$rootScope', 'variantService'
+	    function($location, $scope, $rootScope, variantService) {
+	    	$scope.route = {
+                name: 'variant',
+                collection: 'products',
+                edit: 'product.detail.variant.detail'
+            };
+            
+            angular.extend(this, $controller('AbstractDetailsNodeCtrl', {
+                $scope: $scope,
+                itemService: variantService
+            }));
+            
+	    	$scope.fileUploaded = function ($file, $message, $flow) {
+                var image = JSON.parse($message);
+                $scope.item.medias.push(image);
+                $scope.editSave();
+            };
 	    }
 	]);
