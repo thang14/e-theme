d44c8e2397802e3ddb88cb831e80fea273fabc19
Nguyen Thang <thangn.1411@gmail.com>
2015-11-12 14:37:07 +0700

    Update variant.js

--- js/variant/variant.js
+++ js/variant/variant.js
@@ -10,10 +10,13 @@ var variantModule = angular.module("app.variant", []);
 
 variantModule.factory('variantOption', [
     function() {
-      var variantOption = function() {
-        
+      var variantOption = {
+          options: [
+              'color_name', 
+              'size_name', 
+              'style_name'
+          ]
       }
-      
       return variantOption;
     }
   ]);
