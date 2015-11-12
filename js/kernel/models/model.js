e685152e63d9990a0a280083652e98bedfe6f048
Nguyen Thang <thangn.1411@gmail.com>
2015-11-12 09:16:19 +0700

    Update model.js

--- js/kernel/models/model.js
+++ js/kernel/models/model.js
@@ -8,38 +8,68 @@
  kernelModule.factory('baseModel', ['collectionService'
   function() {
     
+    /**
+     * BaseModel Class
+     */
     var BaseModel = function(data) {
-      this.init(data);
+      this.init();
     }
     
-    BaseModel.collectionName = underfiled;
+    /**
+     * Collection Name
+     */
+    BaseModel.prototype.collectionName = underfiled;
     
+    /**
+     * Gets Collection from collection name
+     */
     BaseModel.getCollection = function() {
       return collectionService.getCollection(this.collectionName);
     }
     
-    BaseModel.prototype.init = function(data) {
+    /**
+     * Init
+     */
+    BaseModel.prototype.init = function() {
       
     }
     
+    /**
+     * Save model
+     */
     BaseModel.prototype.save = function() {
-      return this.getCollection.save(this);
+      if(this.id) {
+        return this.getCollection.save(this);
+      }
+      return this.create(this);
     }
     
-    BaseModel.prototype.create = function() {
-      return this.getCollection.create(this);
+    /**
+     * Create model
+     */
+    BaseModel.prototype.create = function(data) {
+      return this.getCollection.create(data);
     }
     
+    /**
+     * Remove model
+     */
     BaseModel.prototype.remove = function() {
       return this.getCollection.remove(this.id);
     }
     
+    /**
+     * New instance
+     */
     BaseModel.instance = function(data) {
       if(data) {
         return new this(data);
       }
     }
     
+    /**
+     * New instances
+     */
     BaseModel.instances = function(data) {
       var results = [];
       if(data) {
