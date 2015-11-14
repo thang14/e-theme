525dd548536203d1cf5203d2f121fc876ab26790
Nguyen Thang <thangn.1411@gmail.com>
2015-11-14 10:14:08 +0700

    Update add.js

--- js/product/controllers/add.js
+++ js/product/controllers/add.js
@@ -4,24 +4,25 @@
  * @name            OnhanhProduct
  * @description     ProductAddController
  */
- 
+
+var ProductAttributes = {
+    name: "",
+    price: 0,
+    sale: 0,
+    quantity: 1,
+    description_list: [],
+    description: [],
+    keywords: [],
+    attrs: [],
+    shipping: {},
+    variant_options: [],
+    variants: [],
+    status: 'private'
+}
 
 var Controller = function($scope, $rootScope, productService, mediaService, 
  $controller, variantOption, Constants) {
-    $scope.item = {
-        name: "",
-        price: 0,
-        sale: 0,
-        quantity: 1,
-        description_list: [],
-        description: [],
-        keywords: [],
-        attrs: [],
-        shipping: {},
-        variant_options: [],
-        variants: [],
-        status: 'private'
-    };
+    $scope.item = ProductAttributes;
 }
 
 Controller.$inject = [
