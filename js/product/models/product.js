050d1b9a420908a12a554d4bc678835990dfea99
Nguyen Thang <thangn.1411@gmail.com>
2015-11-14 09:30:59 +0700

    Update product.js

--- js/product/models/product.js
+++ js/product/models/product.js
@@ -10,21 +10,15 @@ var ProductModel = Class.extend({
     
 });
 
-    
-	
-	
-	
+
 productModule
 .provider('productModel', Class.extend({
-
-	instance: new NotesModel(),
-
-	/**
-	 * Initialize and configure ActivtyModel
-	 * @return UserModel
- 	*/ 
-	$get:['$location','$route','NotesService',function(productService){
+	
+	instance: new ProductModel(),
+	
+	$get: ['productService', function(productService) {
 		this.instance._service = productService;
 		return this.instance;
 	}]
+	
 }));
