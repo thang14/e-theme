bc8817043656227ad7d9f5cb3c647c653ac6747a
Nguyen Thang <thangn.1411@gmail.com>
2015-11-15 17:14:25 +0700

    Create buttons.js

---
+++ web/product/buttons.js
@@ -0,0 +1,9 @@
+<button type="button" class="btn btn-danger btn-sm text-semibold" ng-click="editCancel()">
+    Hủy
+</button>
+<button type="button" class="btn btn-primary btn-sm text-semibold" ng-click="editSave()">
+    <i class="fa fa-floppy-o"></i>  Lưu
+</button>
+<button type="button" class="btn btn-primary btn-sm text-semibold" ng-click="editSaveAndFinish()">
+    <i class="fa fa-floppy-o"></i>  Lưu và kết thúc
+</button>
