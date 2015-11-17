a0a9d43a0c8a64e45e0da8946106dbbbe9e4b037
Nguyen Thang <thangn.1411@gmail.com>
2015-11-17 15:48:53 +0700

    Update resource.js

--- js/kernel/services/resource.js
+++ js/kernel/services/resource.js
@@ -21,12 +21,33 @@ angular.module('app.kernel')
             });
         };
         
+        /**
+         * CREATE
+         */
         resourceService.prototype.create = function(data) {
             return new this.resource(data);
         }
         
+        /**
+         * GET
+         */
         resourceService.prototype.get = function(params, callback) {
             return new this.resource.get(params, callback);
+        }
+        
+        /**
+         * UPDATE
+         */
+        resourceService.prototype.update = function($id, $data) {
+            return new this.resource.update({id: $id}, $data);
+        }
+        
+        
+        /**
+         * Remove
+         */
+        resourceService.prototype.remove = function($id) {
+            return new this.resource.remove({id: $id}).$remove();
         }
         
         return resourceService;
