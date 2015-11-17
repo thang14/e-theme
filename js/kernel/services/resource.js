c22a3f626bcc1deda947383d13abaee1dff0b75a
Nguyen Thang <thangn.1411@gmail.com>
2015-11-17 11:56:08 +0700

    Update resource.js

--- js/kernel/services/resource.js
+++ js/kernel/services/resource.js
@@ -7,19 +7,6 @@
 angular.module('app.kernel')
 .factory('resourceService', ['Environment', '$resource',
     function(Environment, $resource) {
-        
-        return $resource(':shop/card/:id',{
-            userId:123, 
-            cardId:'@id'
-        }, {
-          charge: {
-              method:'POST', 
-              params:{
-                  charge:true
-                  
-              }}
-        });
-        
         var resourceService = function(name) {
             var api = Environment.settings.api + '/' + name + Environment.settings.prefix;
             return $resource(api+'/:id',{
