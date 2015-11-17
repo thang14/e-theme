76cc228995f150c680c07a360073e820a7e88588
Nguyen Thang <thangn.1411@gmail.com>
2015-11-17 09:18:16 +0700

    Update environment.js

--- js/environment.js
+++ js/environment.js
@@ -8,17 +8,19 @@
 window.angular.module('app.environment', [])
     .service('Environment', function() {
         var api_domain = document.domain.replace("admin", "api");
-
+        
+        function getNamespace() {
+            return 'admin-theme/examples/data';
+            return window.location.pathname.replace(/^\/([^\/]*).*$/, '$1');
+        }
+        
         return {
             settings: {
-                api: 'http://' + api_domain + '/admin-theme/examples/data',
+                namespace: getNamespace(),
+                api: 'http://' + api_domain + '/'+getNamespace(),
                 prefix: ".json",
                 domain: 'http://' + api_domain,
-                pageTitle: 'Onhanh - open source project | Admin'
+                pageTitle: 'Onhanh - open source project | Admin Dasboard'
             },
-
-            currentShop: function() {
-                return window.location.pathname.replace(/^\/([^\/]*).*$/, '$1');
-            }
         }
     });
