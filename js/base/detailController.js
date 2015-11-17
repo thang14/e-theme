5976927808cf931cdce856e9c735853725a63796
Nguyen Thang <thangn.1411@gmail.com>
2015-11-17 16:41:39 +0700

    Update detailController.js

--- js/base/detailController.js
+++ js/base/detailController.js
@@ -5,10 +5,24 @@
  * @description     BaseDetailController
  */
  
+ var VariantDefault = {
+     price: 0,
+     sale: 0,
+     quantity: 0,
+     options: []
+ }
+ 
 var Controller = function($scope, $state, $stateParam, service) {
     $scope.detail = {
         id: $stateParam.id,
         name: $stateParam.name
+    }
+    
+    
+    if($scope.detail.id) {
+        $scope.item = service.get($scope.detail.id);
+    } else {
+        $scope.item = angular.copy(VariantDefault);
     }
 }
 
