e1da545bdbc2195f078a296d9c1f98766ae530c9
Nguyen Thang <thangn.1411@gmail.com>
2015-11-17 11:57:30 +0700

    Update base.js

--- js/kernel/services/base.js
+++ js/kernel/services/base.js
@@ -8,20 +8,10 @@
 kernelModule.service('baseService', ['collectionService',
     function(collectionService) {
         return {
-
             collectionName: undefined,
-
             getCollection: function() {
                 return collectionService.getCollection(this.collectionName);
-            },
-
-            load: function(params) {
-                return this.getCollection().getCollection(params);
-            },
-
-            get: function($id) {
-                return this.getCollection().get($id);
-            },
+            }
         }
     }
 ]);
