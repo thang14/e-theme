46701565b70b4dceac3220a9d68512c0cce76531
Nguyen Thang <thangn.1411@gmail.com>
2015-11-17 16:49:46 +0700

    Update detailController.js

--- js/base/detailController.js
+++ js/base/detailController.js
@@ -18,12 +18,48 @@ var Controller = function($scope, $state, $stateParam, service) {
         name: $stateParam.name
     }
     
-    
+    // Load Detail 
     if($scope.detail.id) {
         $scope.item = service.get($scope.detail.id);
     } else {
         $scope.item = angular.copy(VariantDefault);
     }
+    
+    //Save
+    $scope.save = function() {
+        if(!$scope.detail.id) {
+            $scope.item = service.create($scope.item);
+        }
+        $scope.item.$save();
+    }
+    
+    //Save and finish
+    $scope.saveAndFinish = function() {
+        $scope.save();
+        $state.transitionTo($scope.route.name);
+    }
+    
+    //Cancel
+    $scope.cancel = function() {
+        $state.transitionTo($scope.route.name);
+    }
+    
+    //Clone
+    $scope.newItem = function() {
+        $state.transitionTo($scope.route.name+'.new');
+    }
+    
+    //Clone
+    $scope.clone = function() {
+        
+    }
+    
+    //Clone
+    $scope.remove = function() {
+        $scope.item.$remove();
+        $state.transitionTo($scope.route.name);
+    }
+    
 }
 
 baseModule.controller('baseDetailController', Controller);
