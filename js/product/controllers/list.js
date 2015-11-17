ae6bf43cf9c663000ffb8aa5e06fa7eea8e5da11
Nguyen Thang <thangn.1411@gmail.com>
2015-11-17 13:01:54 +0700

    Update list.js

--- js/product/controllers/list.js
+++ js/product/controllers/list.js
@@ -16,7 +16,7 @@ productModule
                 $scope.currentPage = page;
             }
             
-            $scope.pageChanged = function() {
+            $scope.selectPage = function() {
                 $scope.load();
             }
             
