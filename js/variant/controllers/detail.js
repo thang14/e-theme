220bed67ef09f9d9bba6105a4e6b363e30769a48
Nguyen Thang <thangn.1411@gmail.com>
2015-11-17 16:30:17 +0700

    Update detail.js

--- js/variant/controllers/detail.js
+++ js/variant/controllers/detail.js
@@ -5,8 +5,13 @@
  * @description     VariantDetailController
  */
  
-var Controller = function(){
+var Controller = function(variantService, $controller){
     
+    //Extend
+    angular.extend(this, $controller('baseDetailController', {
+        service: variantService,
+        $scope: $scope
+    }))
 }
 
 variantModule.controller('variantDetailController', Controller);
