948b0bd9ee219d9890f95d47bdf127d98360b0ab
Nguyen Thang <thangn.1411@gmail.com>
2015-11-19 14:02:00 +0700

    Update product.js

--- js/product/models/product.js
+++ js/product/models/product.js
@@ -4,3 +4,28 @@
  * @name            OnhanhProduct
  * @description     ProductModel
  */
+ 
+var ProductModel = function() {
+ this.items = [];
+ this.item = null;
+ this._service = null;
+}
+ 
+/**
+ * Load Product List
+ */
+ProductModel.prototype.load= function(params) {
+  this._service.get(params, function(res) {
+    this.items = res.data;
+  });
+}
+ 
+/**
+ * Load Product List
+ */
+ProductModel.prototype.get= function(id) {
+  this.item = this._service.get({id: id});
+}
+
+
+productModule.factory('productModel', ProductModel);
