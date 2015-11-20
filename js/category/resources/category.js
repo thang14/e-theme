0452d271ec0a72ce168e680db9183610632f81c2
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 16:51:16 +0700

    Update category.js

--- js/category/resources/category.js
+++ js/category/resources/category.js
@@ -9,6 +9,6 @@ categoryModule.factory('categoryResource', [ 'Upload', 'Environment', '$resource
         var api = Environment.settings.domain+'/media';
         
         var categoryResource = $resource(api+'/:id', {id: '@id'});
-        return mediaService;
+        return categoryResource;
     }
 ]);
