83dd2d88ed914181eedf59db71de15aa838fecd1
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 16:34:16 +0700

    Update detail.js

--- js/variant/controllers/detail.js
+++ js/variant/controllers/detail.js
@@ -7,9 +7,18 @@ variantModule.controller('variantDetailController', ['$scope', 'productItem', 'v
           $scope.productItem = productItem;
           $scope.resource = variantItem;
           
+          var goBack = function() {
+               $state.go('product', {id: productItem.id});
+          }
           
           $scope.onSave = function() {
                $scope.productItem.variants.push($scope.resource);
+               goBack();
           }
+          
+          $scope.onCancel = goBack;
+          $scope.onRemove = goBack;
+          
+          
      }
 ]);
