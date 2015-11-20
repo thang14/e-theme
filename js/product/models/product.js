0491dee6e36a1adf1a0deb96d89e71c768da73df
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 14:50:25 +0700

    Update product.js

--- js/product/models/product.js
+++ js/product/models/product.js
@@ -5,69 +5,22 @@
  * @description     ProductModel
  */
  
-
-var Product = function() {
- this.items = [];
- this.total = 0;
- this.current = null;
- 
- this._resource = null;
-}
-
 /**
- * Load Product List
- * @param Object|null params The param builder query
- * @return void(0)
- */
-Product.prototype.$query= function(params, callback) {
-  this._resource.$get(params, function(res) {
-    res.data.forEach(function(item) {
-      this.items.push(new this._resource(item));
-    }, this);
-    this.total = res.total;
-    callback ? callback(this) : '';
-  });
-  
-  return this;
-}
+* Product model, provider since all products
+* in the application use the same model
+*/
+var ProductProvider = function() {
  
-/**
- * Gets product detail
- * @param string id The Id of product item
- * @return void(0)
- */
-Product.prototype.$get= function(id, callback) {
-  if(id != undefined) {
-    this.current = this._resource.get({id: id});
-  } else {
-   this.current = new this._resource();
-  }
-  return this;
+ instance: new BaseModel(),
+ 
+ /**
+  * Initialize and configure ProductModel
+  * @return ProductModel
+  */
+ $get: ['productResource', function(productResource) {
+   this.instance._resource = productResource;
+   return this.instance;
+ }]
 }
+productModule.provider('Product', productResource);
 
-
-
-(function() {
-  
-
-  /**
-   * Product model, provider since all products
-   * in the application use the same model
-   */
-  var ProductProvider = function() {
-    
-    instance: new BaseModel(),
-    
-    /**
-     * Initialize and configure ProductModel
-     * @return ProductModel
-     */
-    $get: ['productResource', function(productResource) {
-      this.instance._resource = productResource;
-      return this.instance;
-    }]
-  }
-  
-  productModule.provider('Product', productResource);
-  
-})
