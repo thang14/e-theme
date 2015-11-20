3ddb5223d555d7f8145daa533cbc1689487e0b24
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 15:07:14 +0700

    Update model.js

--- js/base/model.js
+++ js/base/model.js
@@ -4,8 +4,29 @@
  * @name            OnhanhProduct
  * @description     ProductModel
  */
-productModule.provider('BaseModel', [
-  this.items = [];
-  this.total = [];
-  this.current = 0;
+productModule.factory('Model', [
+  
+  function Model(resource) {
+    this.items = [];
+    this.current = null;
+    this.total = 0;
+    this.resource = resource;
+  }
+  
+  Model.prototype.$get = function(id) {
+    if(id == undefined) {
+      this.current = new this.resource();
+    }
+    this.current = this.resource.$get({id: id});
+  }
+  
+  Model.prototype.$load = function(params) {
+    this.items = [];
+    this.resource.$get(params, function(res) {
+      this.items.push(new this.resource(res.items));
+      this.total = res.total;
+    }.bind(this));
+  }
+  
+  return Model;
 ]);
