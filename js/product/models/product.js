f412eec146205a069168605465309975ae927bbb
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 15:09:07 +0700

    Update product.js

--- js/product/models/product.js
+++ js/product/models/product.js
@@ -4,11 +4,9 @@
  * @name            OnhanhProduct
  * @description     ProductModel
  */
-productModule.provider('Product', ['model', 'productResource',
-  function(productResource) {
-    return angular.extend(model, {
-      resource:productResource
-    })
+productModule.provider('Product', ['Model', 'productResource',
+  function(Model, productResource) {
+    return new Model(productResource);
   }
 ]);
 
