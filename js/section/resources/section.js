6d33f5fa5fe4fbaa873ebd3c8a82d5b9070450c8
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 15:12:42 +0700

    Create section.js

---
+++ js/section/resources/section.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @name            OnhanhProduct
+ * @description     sectionModule
+ */
+sectionModule.factory('sectionResource', ['resourceService'
+    var sectionResource = resourceService('section');
+    return sectionResource;
+]);
