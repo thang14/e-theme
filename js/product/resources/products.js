415bae76570c415b8d59378e43b5b69b84d231b4
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 14:46:11 +0700

    Update products.js

--- js/product/resources/products.js
+++ js/product/resources/products.js
@@ -4,32 +4,7 @@
  * @name            OnhanhProduct
  * @description     ProductService
  */
-productModule.factory('productResource', ['resourceService', 'mediaResource'
-    var Products = resourceService('product');
-    
-    /**
-     * Upload file
-     * @param array $file
-     */
-    Products.prototype.$upload = function($files) {
-        if($files && $files.length > 0) {
-            $files.forEach(function(file) {
-                mediaResource.$upload(file).success( function(data) {
-                    this.medias.push(data);
-                }.bind(this));
-            }, this)
-        }
-    }
-    
-    /**
-     * Remove file
-     */
-    Products.prototype.$removeFile = function(index) {
-        this.medias[index].$remove(function() {
-            this.medias.splice(index, 1);
-        }.bind(this));
-        
-    }
-    
-    return Products;
+productModule.factory('productResource', ['resourceService'
+    var productResource = resourceService('product');
+    return productResource;
 ]);
