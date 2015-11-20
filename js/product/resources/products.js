5cb6b6656fe6c369c2f9a1fec09e98ee650b1ece
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 12:33:30 +0700

    Create products.js

---
+++ js/product/resources/products.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * @name            OnhanhProduct
+ * @description     ProductService
+ */
+productModule.factory('Products', ['resourceService',
+    var Products = resourceService('product');
+]);
