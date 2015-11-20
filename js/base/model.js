4fcaa4c25dc9ad1bcfaa2ab172a3c204eb9e8692
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 14:56:11 +0700

    Create model.js

---
+++ js/base/model.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @name            OnhanhProduct
+ * @description     ProductModel
+ */
+productModule.provider('BaseModel', [
+  this.items = [];
+  this.total = [];
+  this.current = 0;
+]);
