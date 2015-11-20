51d42bb67137d3bfb6d32686e086eed6f4ef2cd6
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 15:21:45 +0700

    Create variant.js

---
+++ js/variant/resources/variant.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @name            OnhanhProduct
+ * @description     variantModule
+ */
+variantModule.factory('variantResource', ['resourceService'
+    var variantResource = resourceService('variant');
+    return variantResource;
+]);
