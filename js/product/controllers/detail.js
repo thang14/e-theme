5dfca6b82a67da7e71ddb801433efa0c6e731f4b
Nguyen Thang <thangn.1411@gmail.com>
2015-11-20 16:22:48 +0700

    Update detail.js

--- js/product/controllers/detail.js
+++ js/product/controllers/detail.js
@@ -16,4 +16,22 @@ Controller.$inject = [
 ];
 
 productModule
-    .controller('productDetailController', ProductDetailController);
+.controller('productDetailController', ['$scope', '$state',  'productItem', 'sections',
+    function($scope, $state, productItem) {
+        
+        $scope.resource = productItem;
+        
+        $scope.sections = sections;
+        
+        
+        //Delete
+        $scop.onDelete = function() {
+            $state.go('product');
+        }
+        
+        //onSaveAndFinish
+        $scope.onSaveAndFinish = function() {
+            $state.go('product');
+        }
+    }
+]);
