fcedce4b1e8d0dec7f8c00deaac65e5626bcba3c
Nguyen Thang <thangn.1411@gmail.com>
2015-11-21 16:25:39 +0700

    Update detail.js

--- js/product/controllers/detail.js
+++ js/product/controllers/detail.js
@@ -11,23 +11,23 @@ productModule
  * Product Detail Controller
  */
 .controller('productDetailController', ['$scope', '$state',  'productItem', 'sections',
-    function($scope, $state, productItem) {
-        
-        $scope.resource = productItem;
-        
-        $scope.sections = sections;
-        
-        //Goback
-        var goBack = function() {
-            $state.go('product');
-        }
-        
-        //Delete
-        $scop.onDelete = goBack;
-        
-        //onSaveAndFinish
-        $scope.onSaveAndFinish = goBack;
+  function($scope, $state, productItem) {
+      
+    $scope.resource = productItem;
+    
+    $scope.sections = sections;
+    
+    //Goback
+    var goBack = function() {
+        $state.go('product');
     }
+    
+    //Delete
+    $scop.onDelete = goBack;
+    
+    //onSaveAndFinish
+    $scope.onSaveAndFinish = goBack;
+  }
 ])
 
  
