7b05831e5a870152599d884a7a3830ec33b31cf2
Nguyen Thang <thangn.1411@gmail.com>
2015-11-21 16:02:15 +0700

    Create options.js

---
+++ js/variant/services/options.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @name            OnhanhProduct
+ * @description     ProductModel
+ */
+productModule.provider('VariantOptions', [
+  function() {
+    var obj = {};
+    
+    object.themes = [
+    
+      // Only
+      ['color_name'],
+      ['size_name'],
+      ['style_name'],
+      ['configure_name'],
+      ['weight_name'],
+      ['cover_tyle_name'],
+      
+      
+    ]
+    return obj;
+  }
+]);
