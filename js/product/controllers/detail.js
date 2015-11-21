327ce069a5dc9f42ae91d77318039b03ee771490
Nguyen Thang <thangn.1411@gmail.com>
2015-11-21 16:42:36 +0700

    Update detail.js

--- js/product/controllers/detail.js
+++ js/product/controllers/detail.js
@@ -30,6 +30,60 @@ productModule
   }
 ])
 
+
+/**
+ * Product Detail Controller
+ */
+.controller('productVariantsController', ['$scope', 'VariantOptions', 'ArrayUtil'
+  function($scope, $state, productItem) {
+      
+    $scope.resource.variants = $scope.resource.variants || [];
+    
+    
+    /**
+     * Select product theme
+     * @param integer| null index key of the themes
+     * @return void;
+     */
+    $scope.selectTheme = function(index) {
+      var options = [];
+      // Reset theme
+      if(index === null) {
+        $scope.resource.theme = null;
+        return;
+      }
+      
+      
+      var maps = ArrayUtil.index($scope.resource.variant_options, 'name');
+      
+      // Options
+      var names = VariantOptions.themes[index];
+      
+      names.forEach(function(values, index) {
+        var result = [];
+        values.forEach(function(value) {
+          if(maps[value] == undefined) {
+            maps[value] = {
+              name: value,
+              label: VariantOptions.labels[value],
+              items: []
+            }
+          }
+          result.push(maps[value]);
+        })
+        options.push(result);
+      });
+      
+      $scope.resource.variant_options = options;
+    };
+    
+    
+    
+    
+  }
+])
+
+
  
  
  
