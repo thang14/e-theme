c598765bda7c3f3425e7f1ac217e47e06d296514
Nguyen Thang <thangn.1411@gmail.com>
2015-11-21 15:48:54 +0700

    Update detail.js

--- js/product/controllers/detail.js
+++ js/product/controllers/detail.js
@@ -4,39 +4,7 @@
  * @name            OnhanhProduct
  * @description     ProductDetailController
  */
-
-// ArrayHelper
-var ArrayHelper = {
-    // Index
-    index: function(arr, field, prefix) {
-        prefix = prefix || '_';
-        return arr.map(function(obj) {
-            var rObj = {};
-            var index = obj[field];
-            if(typeof index === Array) {
-                index = index.join(prefix);
-            }
-            rObj[index] = obj;
-            return rObj;
-        });
-    }
-}
-
-var uploadMedia = function($scope, resource, file, instance) {
-    resource.$upload(file, function(response) {
-        instance.medias.push(response.data);
-        $scope.onUploaded(response);
-    });
-}
-
-var removeMedia = function($scope, resource, file, instance) {
-    file.$remove(function() {
-       var index = instance.medias.indexOf(file); 
-       instance.medias.splice(index, 1);
-       $scope.onRemoveMedia(file);
-    });
-}
-
+ 
 productModule
 
 /**
@@ -62,16 +30,6 @@ productModule
     }
 ])
 
-/**
- * ProductVariantsController
- */
-.controller('productVariantsController', [ 
-    '$scope', 'VariantOption', 'variantResource', 'Media',
-    function($scope, VariantOption, Variant, Media) {
-    
-}]);
- 
- 
  
  
  
