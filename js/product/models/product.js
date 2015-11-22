0bb0df86d527f2e63cabd404f71a8dede87ed716
Nguyen Thang <thangn.1411@gmail.com>
2015-11-22 15:09:50 +0700

    Update product.js

--- js/product/models/product.js
+++ js/product/models/product.js
@@ -8,6 +8,9 @@ productModule.provider('Product', $ProductProvider);
 
 function $ProductProvider() {
   
+  this.resource = null;
+  
+  
   /**
    * @appdoc service
    * @name $cookies
@@ -16,50 +19,7 @@ function $ProductProvider() {
    * Provides read/write access to browser's products.`
    */
   this.$get = ['productResource', function(productResource) {
-    return {
-      /**
-       * @appdoc method
-       * @name Product#get
-       *
-       * @description
-       * Returns the value of given product id
-       *
-       * @param {string} id Id to use for lookup.
-       * @returns {string} Raw product value.
-       */
-      get: function(id) {
-        return productResource.get({id: id});
-      }
-      
-      /**
-       * @appdoc method
-       * @name Product#getAll
-       *
-       * @description
-       * Returns the values of given collection
-       *
-       * @param {string} id Id to use for lookup.
-       * @returns {string} Raw products value.
-       */
-      getAll: function(id) {
-        return productResource.get();
-      }
-      
-      
-      /**
-       * @appdoc method
-       * @name Product#getAll
-       *
-       * @description
-       * Returns the values of given collection
-       *
-       * @param {string} id Id to use for lookup.
-       * @returns {string} Raw products value.
-       */
-      query: function(params) {
-        return productResource.get(params);
-      }
-    }
+    this.resource = productResource;
   }]
   
   
