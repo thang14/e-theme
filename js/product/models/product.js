2cb0b7e7112cae24609a4e44989114eaa6ae4bc4
Nguyen Thang <thangn.1411@gmail.com>
2015-11-22 14:51:48 +0700

    Update product.js

--- js/product/models/product.js
+++ js/product/models/product.js
@@ -8,4 +8,22 @@ productModule.provider('Product', $ProductProvider);
 
 function $ProductProvider() {
   
+  this.current = null;
+  
+  this.items = [];
+  
+  /**
+   * @appdoc service
+   * @name $cookies
+   *
+   * @description
+   * Provides read/write access to browser's products.`
+   */
+  this.$get = [ function() {
+    
+  }]
+  
+  
+  
+  
 }
