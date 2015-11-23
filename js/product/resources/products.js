9374e92440499064c7468b1ab5978f50df683716
Nguyen Thang <thangn.1411@gmail.com>
2015-11-23 10:44:30 +0700

    Update products.js

--- js/product/resources/products.js
+++ js/product/resources/products.js
@@ -8,18 +8,32 @@ productModule.factory('productResource', ['resourceService', 'mediaResource'
     var productResource = resourceService('product');
     
     productResource.prototype.upload = function(file) {
-        var item = this.getItemDefault();
-        item.medias = item.medias || [];
-        item.medias.push(file);
-        file.upload = mediaResource.upload(file);
+        var variant = this.getVariantDefault();
+        variant.medias = variant.medias || [];
+        variant.medias.push(file);
+        variant.upload = mediaResource.upload(file);
         // Success
         file.upload.success(function(data, status, headers, config ) {
             file = new mediaResource(data);
+            if(variant.id) {
+                variant.$save();
+            }
         });
         // Progress
         file.upload.progress(function(evt) {
             file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
         })
+    }
+    
+    productResource.prototype.removeFile = function(file) {
+        var variant = this.getVariantDefault();
+        var index = variant.medias.indexOf(file);
+        file.$remove();
+        variant.medias.splice(index, 1);
+        if(variant.id) {
+            variant.$save();
+        }
+        
     }
     return productResource;
 ]);
