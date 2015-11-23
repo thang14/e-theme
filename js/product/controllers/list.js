6d732fcf48e2ba3edbb4c06009c19120551e485a
Nguyen Thang <thangn.1411@gmail.com>
2015-11-23 08:46:02 +0700

    Update list.js

--- js/product/controllers/list.js
+++ js/product/controllers/list.js
@@ -5,37 +5,37 @@
  * @description     ProductController
  */
 productModule
-    .controller('productController', [ '$scope', '$state', 'productGridService',
-        function($scope, $state, productGridService) {
+.controller('productController', [ '$scope', '$state', 'productGridService',
+    function($scope, $state, productGridService) {
 
-            //Page Init
-            $scope.currentPage = 1;
-            $scope.maxSize = 5;
+        //Page Init
+        $scope.currentPage = 1;
+        $scope.maxSize = 5;
 
-            $scope.setPage = function(page) {
-                $scope.currentPage = page;
-            }
-            
-            $scope.gridOptions = productGridService.gridOptions();
-            
-            
-            // Load Items
-            $scope.load = function() {
-                productGridService.load({
-                    page: $scope.currentPage
-                });
-            }
-            $scope.load();
-            
-            
-            $scope.viewDetail = function(row) {
-                $state.transitionTo('product.detail',{
-                  id:row.entity.id
-                })
-            }
-            
-            $scope.newProduct = function(id) {
-                $state.transitionTo('product.new');
-            }
+        $scope.setPage = function(page) {
+            $scope.currentPage = page;
         }
-    ]);
+        
+        $scope.gridOptions = productGridService.gridOptions();
+        
+        
+        // Load Items
+        $scope.load = function() {
+            productGridService.load({
+                page: $scope.currentPage
+            });
+        }
+        $scope.load();
+        
+        
+        $scope.viewDetail = function(row) {
+            $state.transitionTo('product.detail',{
+              id:row.entity.id
+            })
+        }
+        
+        $scope.newProduct = function(id) {
+            $state.transitionTo('product.new');
+        }
+    }
+]);
