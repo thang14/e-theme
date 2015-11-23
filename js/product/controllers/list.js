f9378790ab85dcc26b05c5dc317baf260e9e2154
Nguyen Thang <thangn.1411@gmail.com>
2015-11-23 08:45:28 +0700

    Update list.js

--- js/product/controllers/list.js
+++ js/product/controllers/list.js
@@ -5,8 +5,8 @@
  * @description     ProductController
  */
 productModule
-    .controller('productController', [ '$scope', 'product', 'gridService', '$state',
-        function($scope, product, gridService, $state) {
+    .controller('productController', [ '$scope', '$state', 'productGridService',
+        function($scope, $state, productGridService) {
 
             //Page Init
             $scope.currentPage = 1;
@@ -15,81 +15,27 @@ productModule
             $scope.setPage = function(page) {
                 $scope.currentPage = page;
             }
-            //Columns
-          $scope.columns = [{
-            name: "id",
-            width: '75',
-            enableColumnMenu: false,
-          },{
-            name: "photo",
-            displayName: "Ảnh",
-            width: '75',
-            cellTemplate: '/web/ui-grid/image-view.html',
-            enableColumnMenu: false,
-          },{
-            enableColumnMenu: false,
-            name: "name",
-            displayName: "Tên",
-            enableCellEdit: true,
-            cellTemplate: '<div class="ngCellText ui-grid-cell-contents">'+
-                '<a href="javascript:void(0)"  ng-click="grid.appScope.viewDetail(row)"><strong>{{MODEL_COL_FIELD}}</strong></a></div>'
-          },{
-            enableColumnMenu: false,
-            name: "price",
-            displayName: "Giá",
-            width: '100',
-            editableCellTemplate: '/web/ui-grid/editor-price.html',
-            enableCellEdit: true,
-            cellTemplate: '<div class="ngCellText ui-grid-cell-contents"><strong>{{MODEL_COL_FIELD | currency:"":0}} đ</strong></div>'
-          },{
-            enableColumnMenu: false,
-            name: "sale",
-            displayName: "Khuyến mãi",
-            width: '80',
-            enableCellEdit: true,
-            cellTemplate: '<div class="ngCellText ui-grid-cell-contents"><strong>{{MODEL_COL_FIELD}} %</strong></div>'
-          },{
-            enableColumnMenu: false,
-            displayName: "Số lượng",
-            name: "quantity",
-            enableCellEdit: true,
-            width: '70',
-            enableCellEdit: true,
-          },{
-            enableColumnMenu: false,
-            displayName: "Trạng thái",
-            name: "status",
-            type: 'boolean',
-            width: '50',
-
-          },{
-            enableColumnMenu: false,
-            name: "action",
-            displayName: "",
-            width: '100',
-            cellTemplate: '/web/collection/action.html',
-          }];
-
-          $scope.onSaveRow = function(rowEntity) {
-            ProductModel._service.update(rowEntity.id, rowEntity);
-          }
-          
-          $scope.gridOptions = gridService.gridOptions($scope, product);
-
-          //load collection from remote
-          $scope.load = function() {
-            product.load({page: $scope.currentPage});
-          }
-
-
-          $scope.viewDetail = function(row) {
-            $state.transitionTo('product.detail',{
-              id:row.entity.id
-            })
-          }
-
-          $scope.newProduct = function(id) {
-            $state.transitionTo('product.new');
-          }
+            
+            $scope.gridOptions = productGridService.gridOptions();
+            
+            
+            // Load Items
+            $scope.load = function() {
+                productGridService.load({
+                    page: $scope.currentPage
+                });
+            }
+            $scope.load();
+            
+            
+            $scope.viewDetail = function(row) {
+                $state.transitionTo('product.detail',{
+                  id:row.entity.id
+                })
+            }
+            
+            $scope.newProduct = function(id) {
+                $state.transitionTo('product.new');
+            }
         }
     ]);
