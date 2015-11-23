55eb69d846485f205ebe154b7cfe862fc84a324e
Nguyen Thang <thangn.1411@gmail.com>
2015-11-23 10:37:27 +0700

    Update products.js

--- js/product/resources/products.js
+++ js/product/resources/products.js
@@ -4,7 +4,22 @@
  * @name            OnhanhProduct
  * @description     ProductService
  */
-productModule.factory('productResource', ['resourceService'
+productModule.factory('productResource', ['resourceService', 'mediaResource'
     var productResource = resourceService('product');
+    
+    productResource.prototype.upload = function(file) {
+        var item = this.getItemDefault();
+        item.medias = item.medias || [];
+        item.medias.push(file);
+        file.upload = mediaResource.upload(file);
+        // Success
+        file.upload.success(function(data, status, headers, config ) {
+            file = new mediaResource(data);
+        });
+        // Progress
+        file.upload.progress(function(evt) {
+            file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        })
+    }
     return productResource;
 ]);
