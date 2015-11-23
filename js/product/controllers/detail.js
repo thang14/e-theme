0104e847a312e5d5744082da2c99a5f9946ac66d
Nguyen Thang <thangn.1411@gmail.com>
2015-11-23 08:32:49 +0700

    Update detail.js

--- js/product/controllers/detail.js
+++ js/product/controllers/detail.js
@@ -61,90 +61,17 @@ productModule
     $scope.resource.variants = $scope.resource.variants || [];
     $scope.resource.variant_options = $scope.resource.variant_options : [];
     
-    // Map data
-    var optionMaps = _.indexBy($scope.resource.variant_options, 'name');
-    var itemMaps = _.indexBy($scope.resource.variants, 'option');
     
-    
-    /**
-     * Select product theme
-     * @param integer| null index key of the themes
-     * @return void;
-     */
     $scope.selectTheme = function(index) {
-      var options = [], 
-          names = VariantOptions.themes[index];
-          
-      // Reset theme
-      if(index === null) {
-        $scope.resource.theme = null;
-        return;
-      }
-      
-      
-      
-      
-      // Options
-      var names = VariantOptions.themes[index];
-      
-      names.forEach(function(values, index) {
-        var result = [];
-        values.forEach(function(value) {
-          if(angular.isUndefined(optionMaps[value])) {
-            optionMaps[value] = {
-              name: value,
-              label: VariantOptions.labels[value],
-              items: []
-            }
-          }
-          result.push(maps[value]);
-        })
-        options.push(result);
-      });
-      
-      $scope.resource.variant_options = options;
+      $scope.resource.selectTheme(index);
     };
     
-    /**
-     * GenerateVariants
-     * @return void;
-     */
-    $scope.generateVariants = function(key, data) {
-      key = key || 0;
-      if(key === 0) {
-        $scope.resource.variants = [];
-      }
-      
-      var options = $scope.resource.variant_options;
-      data = data || [];
-      (options[key].items).forEach(function(value, index) {
-        var item = angular.copy(data);
-        item.push(index);
-        if(!options[key + 1]) {
-          if(angular.isUndefined(itemMaps[item.join('_')])) {
-            itemMaps[item.join('_')] = new variantResource({
-              option: item.join('_'),
-              price: 0,
-              sale: 0,
-              quantity: 1,
-            });
-          }
-          
-          $scope.resource.variants.push(itemMaps[item.join('_')]);
-          return;
-        }
-        $scope.generateVariants(key + 1, item);
-      });
+    $scope.generateVariants = function() {
+      $scope.resource.generateVariants(index);
     };
     
     $scope.removeVariant = function(variant) {
-      var index = $scope.resource.variants.indexOf(variant);
-      if(index >= 0) {
-        if(variant.$id()) {
-          variant.$remove();
-        }
-        $scope.resource.variants.splice(index, 1);
-      }
+      $scope.resource.removeVariant(variant);
     }
   }
 ])
