bedb749f6c84bac90f831018a6959a9881d1d896
Nguyen Thang <thangn.1411@gmail.com>
2015-11-23 11:18:02 +0700

    Update products.js

--- js/product/resources/products.js
+++ js/product/resources/products.js
@@ -62,32 +62,11 @@ productModule.factory('productResource', ['resourceService', 'mediaResource', 'v
         
         
         productResource.prototype.upload = function(file) {
-            var variant = this.getVariantDefault();
-            variant.medias = variant.medias || [];
-            variant.medias.push(file);
-            variant.upload = mediaResource.upload(file);
-            // Success
-            file.upload.success(function(data, status, headers, config ) {
-                file = new mediaResource(data);
-                if(variant.id) {
-                    variant.$save();
-                }
-            });
-            // Progress
-            file.upload.progress(function(evt) {
-                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-            })
+            return this.getVariantDefault().upload(file);
         }
         
         productResource.prototype.removeFile = function(file) {
-            var variant = this.getVariantDefault();
-            var index = variant.medias.indexOf(file);
-            file.$remove();
-            variant.medias.splice(index, 1);
-            if(variant.id) {
-                variant.$save();
-            }
-            
+            return this.getVariantDefault().removeFile(file);
         }
         return productResource;
     }
