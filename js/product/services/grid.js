2b14add3f61698387a36ea20605fa15ed45639f2
Nguyen Thang <thangn.1411@gmail.com>
2015-11-25 15:15:45 +0700

    Update grid.js

--- js/product/services/grid.js
+++ js/product/services/grid.js
@@ -1,1 +1,33 @@
+'use strict';
 
+/**
+ * @name            OnhanhProduct
+ * @description     productModule
+ */
+productModule
+.service("productGrid", function() {
+  return {
+    columns: [{
+      name: "name",
+      displayName: "Tên",
+      cellTemplate: '<div class="ui-grid-cell-contents" title="TOOLTIP"> '+
+          '<a href="#" ng-click="grid.appScope.viewDetail(row.entity.id)">{{COL_FIELD}}'+
+          '</a></div>'
+    }, {
+      name: "price",
+      displayName: "Giá tiền",
+    }, {
+      name: "sale",
+      displayName: "Khuyến mãi",
+    }, {
+      name: "quantity",
+      displayName: "Số lượng",
+    }],
+    
+    gridOptions: function() {
+      return {
+        
+      }
+    }
+  }
+});
