e29ca0e1d0d4f3ebc91fb1de90e9f4b614b4979f
Nguyen Thang <thangn.1411@gmail.com>
2015-11-25 07:16:56 +0700

    Update product.js

--- js/product/product.js
+++ js/product/product.js
@@ -5,5 +5,19 @@
  * @description     ProductModule
  */
 var productModule = angular.module("app.product", [
-  'app.variant',
+  
+  // App Module
+  'app.media',
+  'app.section',
+  'app.kernel',
+  
+  
+  'ui.utils.masks',
+    
+  //Bootstrap
+  'ui.bootstrap',
+
+  //UI.grid
+  'ui.grid',
+  'ui.grid.edit',
 ]);
