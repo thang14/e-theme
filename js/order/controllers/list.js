d824347a0e481a60686171c68d7b7d511566ee0f
Nguyen Thang <thangn.1411@gmail.com>
2015-11-26 08:24:44 +0700

    Update list.js

--- js/order/controllers/list.js
+++ js/order/controllers/list.js
@@ -5,51 +5,8 @@
  * @description     OrderController
  */
 orderModule
-    .controller('orderController', [ '$scope', 'orderService',
-        function($scope, orderService) {
-            $scope.columns = [{
-                name: "id",
-                displayName: "Order code",
-                enableColumnMenu: false,
-                enableSorting: false,
-                enableFiltering: false,
-                width: '75',
-            },{
-                name:"fullname",
-                enableColumnMenu: false,
-                enableSorting: false,
-                enableFiltering: false,
-                width: '200',
-            },{
-                name:"phone",
-                enableColumnMenu: false,
-                enableSorting: false,
-                enableFiltering: false,
-                width: '200',
-            },{
-                name: "price",
-                enableColumnMenu: false,
-                enableSorting: false,
-                enableFiltering: false,
-                width: '200',
-            },{
-                name: "payments",
-                enableColumnMenu: false,
-                enableSorting: false,
-                enableFiltering: false,
-                width: '200',
-            },{
-                name: "created_at",
-                enableColumnMenu: false,
-                enableSorting: false,
-                enableFiltering: false,
-                width: '80',
-            },{
-                name: "status",
-                enableColumnMenu: false,
-                enableSorting: false,
-                enableFiltering: false,
-                width: '80',
-            }];
+    .controller('orderController', [ '$scope', 'gridOptions',
+        function($scope, gridOptions) {
+            $scope.gridOptions = gridOptions;
         }
     ]);
