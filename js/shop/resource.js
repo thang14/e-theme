aa770cb93723f4f1ce1cb858a955434d918422be
Nguyen Thang <thangn.1411@gmail.com>
2015-11-26 08:16:59 +0700

    Create resource.js

---
+++ js/shop/resource.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @name            OnhanhShop
+ * @description     ShopModule
+ */
+shopModule.factory("Shop", [ 'resourceService',
+  function(resourceService) {
+    var Shop = resourceService("");
+  }
+]);
