37063f1ed62c9e3470df1b8f10a2903ab61eebd9
Nguyen Thang <thangn.1411@gmail.com>
2015-11-26 08:09:55 +0700

    Update shop.js

--- js/shop/shop.js
+++ js/shop/shop.js
@@ -5,21 +5,11 @@
  * @description     ShopModule
  */
 var shopModule = angular.module("app.shop", [])
-.run(['$http', '$state', '$rootScope', 'shopService', 'Environment',
-  function($http, $state, $rootScope, shopService, Environment) {
+.run(['$http', '$state', '$rootScope', 'Shop', 'Environment',
+  function($http, $state, $rootScope, Shop, Environment) {
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
       if ($rootScope.shop === undefined) {
-        shopService
-        .getInfomation()
-        .success(function(data, status) {
-          if(data.name) {
-            $rootScope.shop = data;
-          } else {
-            $rootScope.shop = false;
-            event.preventDefault();
-            $state.transitionTo('404');
-          }
-        });
+        $rootScope.shop = Shop.get({slug: Environment.settings.namespace});
       } else if($rootScope.shop == false) {
         event.preventDefault();
         $state.transitionTo('404');
