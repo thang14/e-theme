41b0484934c863a510d66aac7af52a84900f59ab
Nguyen Thang <thangn.1411@gmail.com>
2015-11-26 07:57:49 +0700

    Create info.js

---
+++ js/settings/controllers/info.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @name            OnhanhSettings
+ * @description     settingInfoController
+ */
+settingsModule.controller('settingInfoController', [ '$scope',
+    function($scope) {
+        
+    }
+]);
