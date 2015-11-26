38cccf1e4504535d791d0b20bf91f681cf2a1cd6
Nguyen Thang <thangn.1411@gmail.com>
2015-11-26 07:56:59 +0700

    Create manager.js

---
+++ js/settings/controllers/manager.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @name            OnhanhSettings
+ * @description     settingsModule
+ */
+settingsModule
+    .controller('settingManagerController', [ '$scope',
+        function($scope) {
+            
+        }
+    ]);
