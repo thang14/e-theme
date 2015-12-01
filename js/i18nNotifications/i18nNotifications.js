7509f90ec9b65f46c87f9e0326708e9faebc1661
Nguyen Thang <thangn.1411@gmail.com>
2015-12-01 19:03:04 +0700

    Update i18nNotifications.js

--- js/i18nNotifications/i18nNotifications.js
+++ js/i18nNotifications/i18nNotifications.js
@@ -1,1 +1,14 @@
+'use strict';
 
+/**
+ * @name            OnhanhProduct
+ * @description     ProductModule
+ */
+var productModule = angular.module("app.product", [
+    'ui.router',
+    'app.media',
+    'app.section',
+    'ui.utils.masks',
+    'ngTagsInput',
+    'cgNotify',
+]);
